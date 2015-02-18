b301e21be951700dfc6185fad1dee468acda78e4
Ofan <sofandani@gmail.com>
2015-02-18 17:37:20 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kuas_db_v1');
+define('DB_NAME', '****');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '****');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '****');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
